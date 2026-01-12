5d045e770f55d87485309e9cb2e80f0bedf8d96f
Daniel Reyes <danielsoftwaredev@gmail.com>
2026-01-11 23:31:17 -0600

    07: projects updated

--- src/components/home/Skills/skill-list.ts
+++ src/components/home/Skills/skill-list.ts
@@ -4,21 +4,25 @@ export const skillList = [
     name: "UX/UI Design",
     description:
       "Intuitive and visually appealing user interfaces and experiences.",
+    color: "text-rose-400",
   },
   {
     icon: "services/web",
     name: "Web Apps",
     description: "Websites and web applications tailored to your needs.",
+    color: "text-indigo-400",
   },
   {
     icon: "services/mobile",
     name: "Mobile Apps",
     description: "Cross-platform mobile applications for iOS and Android.",
+    color: "text-teal-400",
   },
   {
     icon: "services/seo",
     name: "SEO Optimization",
     description:
       "Improve your website's visibility and ranking in search engines.",
+    color: "text-orange-400",
   },
 ];
--- src/components/home/projects/project-list.ts
+++ src/components/home/projects/project-list.ts
@@ -1,21 +1,31 @@
-import starwarsApi from "@/images/starwars-api.png";
-import spotifyClone from "@/images/spotify-clone.png";
+import starwarsApi from "@/images/starwars.png";
+import spokeet from "@/images/spokeet.png";
 import musicSaasApp from "@/images/music-saas-app.png";
 
-export const projectList = [
+type Project = {
+  image: ImageMetadata;
+  name: string;
+  description: string;
+  href: string;
+};
+
+export const projectList: Project[] = [
   {
     image: starwarsApi,
     name: "Starwars API",
+    description: "Information about Star Wars universe",
     href: "https://starwars-api-nine.vercel.app/",
   },
   {
-    image: spotifyClone,
-    name: "Music UX-UI",
-    href: "https://spotifyclone-danielsoftware.vercel.app/",
+    image: spokeet,
+    name: "Spokeet",
+    description: "Text to speech AI SaaS",
+    href: "https://voice-saas-app.vercel.app",
   },
   {
     image: musicSaasApp,
-    name: "Songs Generator SaaS App",
+    name: "Retokie",
+    description: "Songs generator AI SaaS",
     href: "https://www.retokie.com/",
   },
 ];
