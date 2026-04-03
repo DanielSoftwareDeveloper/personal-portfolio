df06b18a8c66cb490910f65e2f439de9db2625de
Daniel Reyes <danielsoftwaredev@gmail.com>
2026-04-03 02:52:41 -0600

    description updated

--- src/components/home/projects/project-list.ts
+++ src/components/home/projects/project-list.ts
@@ -1,6 +1,6 @@
 import starwarsApi from "@/images/starwars.png";
+import mochiAssistant from "@/images/mochi-assistant.png";
 import spokeet from "@/images/spokeet.png";
-import musicSaasApp from "@/images/music-saas-app.png";
 
 type Project = {
   image: ImageMetadata;
@@ -13,6 +13,14 @@ type Project = {
 export const projectList: Project[] = [
   {
     image: starwarsApi,
+    name: "Star Wars API",
+    description:
+      "AI music generation platform that creates original songs from simple prompts, allowing users to experiment with melodies, styles, and lyrics.",
+    href: "https://starwars-api-nine.vercel.app/",
+    github: "https://github.com/DanielSoftwareDeveloper/starwars-api",
+  },
+  {
+    image: mochiAssistant,
     name: "Mochi Assistant",
     description:
       "AI assistant that can be integrated into any website to answer questions, guide visitors, and provide instant support using custom business knowledge.",
@@ -26,13 +34,5 @@ export const projectList: Project[] = [
       "AI-powered text-to-speech platform that transforms written text into natural sounding voices for content creators, apps, and videos.",
     href: "https://voice-saas-app.vercel.app",
     github: "https://github.com/DanielSoftwareDeveloper/voice-saas-app",
-  },
-  {
-    image: musicSaasApp,
-    name: "Retokie",
-    description:
-      "AI music generation platform that creates original songs from simple prompts, allowing users to experiment with melodies, styles, and lyrics.",
-    href: "https://www.retokie.com/",
-    github: "https://github.com/tuusuario/mochi-assistant",
   },
 ];
